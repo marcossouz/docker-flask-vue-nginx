b354c0bd5db0bdc076543783a40ff92b13e106d4
Marcos Souza <mrcsz.m@gmail.com>
2020-10-18 11:48:46 -0300

    alterando a porta da api no nginx para que o frontend fique na 80

--- flaskapp/app/app.py
+++ flaskapp/app/app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 application = Flask(__name__)
+CORS(application)
 
 application.config["MONGO_URI"] = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ["MONGODB_PASSWORD"] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE'] 
 
